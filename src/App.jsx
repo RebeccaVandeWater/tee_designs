4c04d5798cf00a069e1b8779a7164a0606a78ca5
Rebecca VandeWater <123334409+RebeccaVandeWater@users.noreply.github.com>
2023-09-29 12:01:49 -0600

    all packages and dependencies working?

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>
+      <h1 className='head-text'>
         React App
       </h1>
     </div>
