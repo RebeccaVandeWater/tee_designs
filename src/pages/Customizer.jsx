f920e0cc4b9c2bc2c2192344627d1553013296cb
Rebecca VandeWater <123334409+RebeccaVandeWater@users.noreply.github.com>
2023-09-29 13:35:46 -0600

    resolved import issues?

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config/config';
+import state from '../store';
+import { download } from '../assets'
+import { downloadCanvasToImage, reader } from '../config/config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/config/constants';
 
 const Customizer = () => {
   return (
