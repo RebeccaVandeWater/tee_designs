f34ba5f6cceb9c65f150ae6811c44567b65d4985
Rebecca VandeWater <123334409+RebeccaVandeWater@users.noreply.github.com>
2023-10-02 13:43:59 -0600

    figured out shadows

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useRef} from 'react';
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber';
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
+import * as THREE from 'three';
 
 const Backdrop = () => {
+  const shadows = useRef();
+
   return (
-    <div>Backdrop</div>
+    <AccumulativeShadows ref={shadows} temporal color='#0f0050' frames={60} alphaTest={1.5} scale={10} rotation={[Math.PI / 2, 1, 0]} position={[0, 0, -0.14]}>
+      <RandomizedLight amount={4} radius={7} intensity={2.5} ambient={0.35} position={[5, 5, -10]} />
+      <RandomizedLight amount={4} radius={6} intensity={2.5} ambient={0.55} position={[-5, 5, -9]} />
+    </AccumulativeShadows>
   )
 }
 
